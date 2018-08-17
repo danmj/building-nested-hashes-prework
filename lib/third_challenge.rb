28cef4d44f7d1230f7216ce858132e52d7ff64a0
danmj <daniel.jung@stern.nyu.edu>
2018-08-17 20:35:00 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -24,6 +24,7 @@ def third_challenge
         #your key/value pairs here
         },
       :heroine => {
+        name: "Juliet", age: "15", status: "alive"
         #your key/value pairs here
         },
       :heroine_friends => []
